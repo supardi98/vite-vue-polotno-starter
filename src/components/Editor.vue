ed999e411d247ab77a7122dadcce91e9de61ab66
Supardi <59808981+supardi98@users.noreply.github.com>
2024-09-22 16:55:19 +0000

    Initial commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { createEditor } from '../../editor';
+import {  onMounted } from 'vue'
+
+
+onMounted(() => {
+  createEditor({
+    container: document.getElementById('editor'),
+  })
+})
+
+</script>
+
+<template>
+  <link
+    href="https://unpkg.com/@blueprintjs/core@5/lib/css/blueprint.css"
+    rel="stylesheet"
+  />
+  <div id="editor"></div>
+</template>
+
+<style scoped>
+#editor {
+  margin: 5%;
+  width: 90%;
+  height: 80vh;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
